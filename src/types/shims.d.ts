8f2d82b4e928a5d001e8bef0c2caeb4e7150c2f4
Paolo Vullo <paolovullo@hotmail.it>
2026-01-16 17:18:50 +0100

    chore(ts): update shim comment

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -1,2 +1,2 @@
 // Intentionally left empty.
-// Typings for the i18n context are provided by `src/i18n/I18nContext.d.ts`.
+// Typings for the i18n context are provided by `src/i18n/I18nContext.ts` (typed TS wrapper).
